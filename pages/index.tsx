15c7b6c865e55982a24981a7a2413fd28bbdf0b0
Matt Condon (shrugs) <mattgcondon@gmail.com>
2021-02-04 14:51:15 -0700

    feat: update landing, silence github

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,4 @@
-import {
-  Code,
-  Divider,
-  Heading,
-  Link,
-  ListItem,
-  OrderedList,
-  Text,
-  UnorderedList,
-  VStack,
-} from '@chakra-ui/react';
+import { Divider, Heading, Link, ListItem, Text, UnorderedList, VStack } from '@chakra-ui/react';
 import { Pill } from 'client/components/Pill';
 
 export default function Home() {
@@ -16,114 +6,65 @@ export default function Home() {
     <VStack minHeight="100vh" py={16} spacing={16} width="full" maxWidth="2xl" mx="auto">
       <VStack px={8} spacing={16} align="start">
         <Heading as="h1" fontSize={['2xl', '3xl', '4xl', '5xl']}>
-          📦 drop.nifti.es
+          <Pill prefix="✋">themanymatts</Pill>
         </Heading>
         <Heading as="h2" fontSize={['md', 'lg', 'xl']} lineHeight="tall">
-          <Pill prefix="📦">Drops</Pill>, <Pill prefix="🚚">digital delivery</Pill>, and{' '}
-          <Pill prefix="↪">distribution</Pill> for ERC1155 tokens.
+          <Link href="https://twitter.com/mattgcondon" isExternal>
+            <Pill prefix="👀">Matt Condon</Pill>
+          </Link>{' '}
+          has a chip in his hand that dispenses rare, digital matt stickers... but only if you find
+          him IRL.
         </Heading>
-        <VStack align="start" spacing={4}>
-          <Heading as="span" fontSize="md" lineHeight="normal">
-            Literally just the easiest way to distribute ERC1155 assets to users, whether
-            they&apos;re familiar with blockchain or not.
-          </Heading>
-          <Heading as="span" fontSize="md" lineHeight="tall" fontWeight="normal">
-            Use it to drop memorabilia like{' '}
-            <Link href="https://themanymatts.lol" isExternal>
-              <Pill prefix="✋">themanymatts</Pill>
-            </Link>{' '}
-            , deliver digital merchandise during a{' '}
-            <Link href="https://softspot.art" isExternal>
-              <Pill prefix="✺">Softspot</Pill>
-            </Link>{' '}
-            event, or distribute token rewards to your users.
-          </Heading>
-          <Heading as="span" fontSize="md" lineHeight="normal" fontWeight="normal">
-            drop.nifti.es uses <sup>(🔜)</sup> the{' '}
-            <Link href="https://opengsn.org" isExternal>
-              Open Gas Network
-            </Link>{' '}
-            for gas-less metatransactions.
-          </Heading>
-        </VStack>
+        <Heading as="h2" fontSize="md" lineHeight="tall">
+          there are many <Pill prefix="❏">matts</Pill>, but one of them could be yours.
+        </Heading>
       </VStack>
       <Divider />
       <VStack px={8} spacing={8} align="start">
+        <Heading as="h3">wut on earth</Heading>
         <Heading as="h4" fontSize="base" lineHeight="tall" fontWeight="normal">
-          drop.nifti.es is an alpha release – if you&apos;d like to use it, bug{' '}
-          <Link href="https://twitter.com/mattgcondon" isExternal>
-            <Pill prefix="🐦">@mattgcondon</Pill>
-          </Link>{' '}
-          on twitter
+          themanymatts are rare digital stickers, proof-of-matt-proximity, and weird art objects.
         </Heading>
-        <Heading as="h4" fontSize="base" lineHeight="tall">
-          integrate drop.nifti.es in three steps <sup>🔜</sup>
-        </Heading>
-        <OrderedList spacing={4}>
+        <UnorderedList spacing={4}>
           <ListItem>
-            Connect drop.nifti.es to your contract,
+            you can only get a sticker by finding me IRL
             <UnorderedList>
+              <ListItem>find me personally and scan my hand, or</ListItem>
               <ListItem>
-                deploy an ERC1155 contract with minting capabilities <sup>(if necessary)</sup>
-              </ListItem>
-              <ListItem>link your signer and ERC1155 contract here on drop.nifti.es.</ListItem>
-              <ListItem>
-                allow drop.nifti.es to mint and transfer assets with
-                <Code>setApprovalForAll(...)</Code> <sup>(if necessary)</sup>
-              </ListItem>
-              <ListItem>
-                fund the{' '}
-                <Link href="https://opengsn.org" isExternal>
-                  Gas Station Network&apos;s RelayHub
+                find one of the many matt stickers around the world and send it to me on{' '}
+                <Link href="https://twitter.com/mattgcondon" isExternal>
+                  <Pill prefix="🐦">Twitter</Pill>
                 </Link>{' '}
-                to pay for your users&apos; gas.
+                or{' '}
+                <Link href="https://instagram.com/oneofthemanymatts" isExternal>
+                  <Pill prefix="📸">Instagram</Pill>
+                </Link>
               </ListItem>
             </UnorderedList>
           </ListItem>
           <ListItem>
-            Sign a JWT — drop.nifti.es supports minting and transfering ERC1155 assets
-            <UnorderedList>
-              <ListItem>
-                mint: <Code>{`{ type: 'mint', ids: [1], amounts: [1] }`}</Code>
-              </ListItem>
-              <ListItem>
-                transfer: <Code>{`{ type: 'transfer', ids: [2, 6], amounts: [1, 7] }`}</Code>
-              </ListItem>
-            </UnorderedList>
+            <Pill prefix="❏">matts</Pill> are the first non-fungible social token
           </ListItem>
           <ListItem>
-            Forward a link to your beneficiary, perhaps in a nice email or something
-            <UnorderedList>
-              <ListItem>
-                <Code>{`https://drop.nifti.es/:token`}</Code>
-              </ListItem>
-            </UnorderedList>
+            ...and maybe something special will come to those who own a <Pill prefix="❏">matt</Pill>
           </ListItem>
-          <ListItem>That&apos;s it.</ListItem>
-        </OrderedList>
+        </UnorderedList>
       </VStack>
       <Divider />
-      <VStack align="start" spacing={16} px={8}>
+      <VStack align="start" spacing={8} px={8}>
+        <Heading as="h3">gotta hand it to &apos;em</Heading>
         <Text textStyle="highlightable" lineHeight="taller">
-          📦 drop.nifi.es is a{' '}
+          <Pill prefix="✋">themanymatts</Pill> is a{' '}
           <Link href="https://nifti.es" isExternal>
             <Pill prefix="❏">nifti.es</Pill>
           </Link>{' '}
           project by
           <Link href="https://twitter.com/mattgcondon" isExternal>
             <Pill prefix="🐦">Matt Condon</Pill>
-          </Link>{' '}
-          for{' '}
-          <Link href="https://themanymatts.lol" isExternal>
-            <Pill prefix="✋">themanymatts</Pill>.
-          </Link>{' '}
-          and{' '}
-          <Link href="https://softspot.art" isExternal>
-            <Pill prefix="✺">Softspot</Pill>
           </Link>
           . You can view, edit, and audit the code on{' '}
-          <Link href="https://github.com/xlnt/drop.nifti.es" isExternal>
-            <Pill prefix="📄">GitHub</Pill>
+          <Link href="https://github.com/shrugs/themanymatts" isExternal>
+            <Pill prefix="📂">GitHub</Pill>
           </Link>
           .
         </Text>
